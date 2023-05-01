e14ea6f820da619853f04d18200230ef8e8c7bda
Sofiapglez <126815471+Sofiapglez@users.noreply.github.com>
2023-05-01 19:26:25 +0100

    clase PokemonLegendario

---
+++ src/PokemonLegendario.java
@@ -0,0 +1,37 @@
+public class PokemonLegendario extends Pokemon{
+    private String localizacion;
+
+
+    public PokemonLegendario(String nombre, String tipo, double altura, double peso, String descripcion) {
+        super(nombre, tipo, altura, peso, descripcion);
+        this.localizacion = localizacion;
+    }
+
+    public PokemonLegendario(String nombre, String tipo, String localizacion) {
+        super(nombre, tipo);
+        this.localizacion = localizacion;
+    }
+
+    public PokemonLegendario(String nombre, String tipo) {
+        super(nombre, tipo);
+    }
+
+    public String getLocalizacion() {
+        return localizacion;
+    }
+
+    public void setLocalizacion(String localizacion) {
+        this.localizacion = localizacion;
+    }
+
+    @Override
+    public String toString() {
+        String msg = super.toString() + "\nLocalización: ";
+        if (localizacion != null) {
+            msg += localizacion;
+        } else {
+            msg += "???";
+        }
+        return msg;
+    }
+}
