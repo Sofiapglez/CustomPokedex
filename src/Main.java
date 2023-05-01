644496fcf6b06b4c5a23b88065dad552bdb0ffbf
Sofiapglez <126815471+Sofiapglez@users.noreply.github.com>
2023-05-01 19:48:19 +0100

    switch del menú interactivo

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,70 @@ public class Main {
             System.out.println("f. Cerrar la pokedex");
             opcion = scanner.nextLine();
 
+            switch (opcion) {
+                case "a":
+                    System.out.println("Introduce el nombre del Pokemon: ");
+                    String nombre = scanner.nextLine();
+                    System.out.println("Introduce el tipo");
+                    String tipo = scanner.nextLine();
+
+                    Pokemon pokemonAvistado = new Pokemon(nombre, tipo);
+                    listaAvistados.agregarPokemon(pokemonAvistado);
+                    System.out.println("Avistamiento añadido correctamente");
+                    break;
+                case "b":
+                    System.out.println("Ingrese el nombre del Pokemon legendario: ");
+                    String nombreLegendario = scanner.nextLine();
+                    System.out.println("Ingrese el tipo de pokemon legendario: ");
+                    String tipoLegendario = scanner.nextLine();
+                    System.out.println("Ingresa la localización del pokemon legendario: ");
+                    String localizacion = scanner.nextLine();
+
+                    PokemonLegendario pokemonLegendario = new PokemonLegendario(nombreLegendario, tipoLegendario);
+                    pokemonLegendario.setLocalizacion(localizacion);
+                    listaAvistados.agregarPokemon(pokemonLegendario);
+                    System.out.println("Avistamiento legendario añadido");
+                    break;
+                case "c":
+                    System.out.println("Introduce el nombre del Pokemon que quieres capturar");
+                    String nombreCaptura = scanner.nextLine();
+                    Pokemon pokemonCaptura = listaAvistados.buscarPokemon(nombreCaptura);
+                    if (pokemonCaptura != null) {
+                        listaAvistados.eliminarPokemon(nombreCaptura);
+                        System.out.print("Introduce el peso del pokemon: ");
+                        double peso = scanner.nextDouble();
+                        System.out.print("Introduce la altura del pokemon: ");
+                        double altura = scanner.nextDouble();
+                        scanner.nextLine();
+                        System.out.print("Introduce la descripción del pokemon: ");
+                        String descripcion = scanner.nextLine();
+
+                        pokemonCaptura.setPeso(peso);
+                        pokemonCaptura.setAltura(altura);
+                        pokemonCaptura.setDescripcion(descripcion);
+
+                        listaCapturados.agregarPokemon(pokemonCaptura);
+                        System.out.println("Pokemon capturado correctamente");
+
+                    } else {
+                        System.out.println("No se ha encontrado el pokemon en la lista de avistados.");
+                    }
+                    break;
+                case "d":
+                    System.out.println("\nLista de pokemons avistados");
+                    listaAvistados.mostrarLista();
+                    break;
+                case "e":
+                    System.out.println("\nLista de pokemons capturados");
+                    listaCapturados.mostrarLista();
+                    break;
+                case "f":
+                    System.out.println("Adiós");
+                    break;
+                default:
+                    System.out.println("Elige una opción válida");
+                    break;
+                }
+            }
         }
     }
-}
