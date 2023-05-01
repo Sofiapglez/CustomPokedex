15de25cee51b257b11fd99ffdecf00cdff3f998f
Sofiapglez <126815471+Sofiapglez@users.noreply.github.com>
2023-05-01 18:55:30 +0100

    atributos, getters, setter, constructor

---
+++ src/Pokemon.java
@@ -0,0 +1,52 @@
+public class Pokemon {
+    private String nombre;
+    private String tipo;
+    private double altura;
+    private double peso;
+    private String descripcion;
+
+    public Pokemon(String nombre, String tipo, double altura, double peso, String descripcion) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.altura = altura;
+        this.peso = peso;
+        this.descripcion = descripcion;
+    }
+
+    public Pokemon(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
